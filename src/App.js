4e87f82485cd738df547a3199dad26b7f1780cf4
HarmanBoparai <erharmanboparai@gmail.com>
2022-11-16 23:58:55 -0500

    created the layout of the portfolio

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import "./styles/Header.css";
+import "./styles/Navbar.css";
+import "./styles/AboutMe.css"
+
+const App = () => <PortfolioContainer />;
+
+export default App;
