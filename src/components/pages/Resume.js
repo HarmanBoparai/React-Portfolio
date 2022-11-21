e59a0407777664fe5c62176d16b1a78b753ac265
HarmanBoparai <erharmanboparai@gmail.com>
2022-11-20 23:13:28 -0500

    added recent project images in assets and deleted the old ones

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
+       <div className="bg-info">
+      <div className="px-2">
+      <h1 className="text-center">Resume</h1>
+   
       <p>
    
       </p>
+    </div>
     </div>
   );
 }
