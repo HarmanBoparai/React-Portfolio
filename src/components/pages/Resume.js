9bc859d6917342d4779aa3e25fbae96c15d25d5a
HarmanBoparai <erharmanboparai@gmail.com>
2022-11-22 22:55:39 -0500

    added resume in the resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,15 +1,19 @@
 import React from 'react';
-
+import resume from '../../assets/resume.png';
 export default function Resume() {
   return (
        <div className="bg-info">
       <div className="px-2">
       <h1 className="text-center">Resume</h1>
-   
-      <p>
-   
-      </p>
-    </div>
+      <a href="harmansresume.pdf"
+  download="harmansresume.pdf">
+   <button className="resumebtn">Download My Resume</button>
+   </a>
+      <img className="resume"
+        src={resume}
+        alt="biography"
+      ></img>
+   </div>
     </div>
   );
 }
